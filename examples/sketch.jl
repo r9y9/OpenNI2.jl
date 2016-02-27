59c5ac72af54d7d724aa23d7de767556d0ab564a
Ryuichi YAMAMOTO <zryuichi@gmail.com>
2016-02-27 15:13:26 +0900

    Add my debugging code

---
+++ examples/sketch.jl
@@ -0,0 +1,65 @@
+using Cxx
+using OpenNI2
+using OpenCV
+
+const ni2 = OpenNI2
+
+isesc(key) = key == 27
+genfilename(ext=".png") =
+    joinpath(dirname(@__FILE__), string(now(), "_", time_ns(), ext))
+
+always_save = false
+const w = 640
+const h = 480
+const pxfmt = ni2.PIXEL_FORMAT_DEPTH_1_MM
+
+ni2.initialize()
+device = ni2.OpenNIDevice()
+ni2.open(device)
+ni2.setDepthColorSyncEnabled(device, false)
+
+depth = ni2.VideoStream()
+ni2.create(depth, device, ni2.SENSOR_DEPTH)
+ni2.setMirroringEnabled(depth, true)
+
+modes = ni2.getSupportedVideoModes(ni2.getSensorInfo(device, ni2.SENSOR_DEPTH))
+for mode in modes
+    if Int(ni2.getResolutionX(mode)) == w &&
+            Int(ni2.getResolutionY(mode)) == h &&
+            ni2.getPixelFormat(mode) == pxfmt
+        println("$w x $h, PIXEL_FORMAT_DEPTH_1_MM: video mode found")
+        ni2.setVideoMode(depth, mode)
+        break
+    end
+end
+
+ni2.start(depth)
+
+frame = ni2.VideoFrameRef()
+
+while true
+    ni2.waitForAnyStream([depth])
+    ni2.readFrame(depth, frame)
+    arr = convert(Array{Cushort,2}, frame)
+    @assert size(arr) == (w, h)
+
+    # [0,1]
+    scaledarr = scale(arr, 1/maximum(arr))
+    cv2.imshow("depth", scaledarr)
+
+    key = cv2.waitKey(delay=1)
+    isesc(key) && break
+
+    if key == 's' || always_save
+        scale!(scaledarr, 255.)
+        fname = genfilename()
+        @show fname
+        cv2.imwrite(fname, cv2.Mat(scaledarr))
+    end
+end
+
+ni2.stop(depth)
+ni2.close(device)
+ni2.shutdown()
+
+cv2.destroyAllWindows()
